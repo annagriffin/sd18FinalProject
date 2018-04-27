b2f0fdd3d1682db4c61c27769eb47f3c0f769688
Anna Griffin <anna.griffin@students.olin.edu>
2018-04-27 14:36:25 -0400

    updating routes

--- flaskexperiment3.py
+++ flaskexperiment3.py
@@ -3,11 +3,12 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField, IntegerField
 from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'Thisissupposedtobesecret!'
@@ -16,36 +17,46 @@ Bootstrap(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view = '/'
 
 
 class User(UserMixin, db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
+    # activity_list = db.Column(db.String(80))
+
+class Activity(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Text)
+    activity_name = db.Column(db.String(15))
+    goal_time = db.Column(db.Integer)
+    progress = db.Column(db.Integer)
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 class LoginForm(FlaskForm):
-    username = StringField("username", validators=[InputRequired(), Length(min=4, max=15)])
-    password = StringField("password", validators=[InputRequired(), Length(min=4, max=80)])
-    remember = BooleanField("remember me")
+    username = StringField("Username", validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=4, max=80)])
+    remember = BooleanField("Remember me")
 
 class RegisterForm(FlaskForm):
-    email = StringField("email", validators=[InputRequired(), Email(message="Indalid Email"), Length(max=50)])
-    username = StringField("username", validators=[InputRequired(), Length(min=4, max=15)])
-    password = StringField("password", validators=[InputRequired(), Length(min=4, max=80)])
+    email = StringField("Email", validators=[InputRequired(), Email(message="Invalid Email"), Length(max=50)])
+    username = StringField("Username", validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=4, max=80)])
 
+class ActivitiesForm(FlaskForm):
+    activity_name = StringField("Activity", validators=[InputRequired(), Length(min=1, max=100)])
+    goal_time = IntegerField("Time", validators=[InputRequired()])
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
     form = LoginForm()
 
     if form.validate_on_submit():
@@ -55,9 +66,25 @@ def login():
                 login_user(user, remember=form.remember.data)
                 return redirect(url_for('dashboard'))
         return '<h1>Invalid username or password</h1>'
-        # return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
 
-    return render_template('login.html', form=form)
+        # return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+    return render_template('index.html', form=form)
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     form = LoginForm()
+#
+#     if form.validate_on_submit():
+#         print('Worked')
+#         user = User.query.filter_by(username=form.username.data).first()
+#         if user:
+#             if check_password_hash(user.password, form.password.data):
+#                 login_user(user, remember=form.remember.data)
+#                 return redirect(url_for('dashboard'))
+#         return '<h1>Invalid username or password</h1>'
+#         return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+#
+#     return render_template('login.html', form=form)
 
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -69,7 +96,7 @@ def signup():
         new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
-        return '<h1>New user has been created</h1>'
+        return redirect(url_for('index'))
         # return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
 
     return render_template('signup.html', form=form)
@@ -77,7 +104,19 @@ def signup():
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template('dashboard.html', user=current_user.username)
+
+@app.route('/add_activity', methods=['GET', 'POST'])
+@login_required
+def add_activity():
+    form = ActivitiesForm()
+    if form.validate_on_submit():
+        new_activity = Activity(user_id=str(current_user), activity_name=form.activity_name.data, goal_time=form.goal_time.data, progress=0)
+        db.session.add(new_activity)
+        db.session.commit()
+        return redirect(url_for('dashboard'))
+
+    return render_template('activitiesform.html', form=form)
 
 @app.route('/logout')
 @login_required
