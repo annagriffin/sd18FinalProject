4d0e48dbba3eabdefab235d441f1e6557d28288c
Anna Griffin <anna.griffin@students.olin.edu>
2018-05-01 14:46:18 -0400

    adding doctrings

--- flaskexperiment3.py
+++ flaskexperiment3.py
@@ -1,5 +1,6 @@
-
-# https://www.youtube.com/watch?v=8aTnmsDMldY
+# We used a tutorial by Printed Pretty to help us structure our FlaskLogin system
+# https://www.com/watch?v=8aTnmsDMldY
+""" This is the python file of the TimeTracker app. """
 
 from flask import Flask, render_template, redirect, url_for,request
 from flask_bootstrap import Bootstrap
@@ -9,6 +10,7 @@ from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
 
 
 app = Flask(__name__)
@@ -22,6 +24,9 @@ login_manager.login_view = '/'
 
 
 class User(UserMixin, db.Model):
+    """ This class defines a user and stores users' infromation in a table in
+    a database. When a user makes an account, their username, email, and password
+    get stored in the datbase. """
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
@@ -30,6 +35,9 @@ class User(UserMixin, db.Model):
 
 
 class Activity(db.Model):
+    """ This class defines an activity object. An activity consists of a name,
+    goal time, and current progress. The user_id  is also included in this object
+    so that it is easily accessible when filterting activites by users."""
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Text)
@@ -42,23 +50,38 @@ class Activity(db.Model):
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 class LoginForm(FlaskForm):
+    """ This class uses FlaskForm and creates a form on the login page. There are
+    entries for the username and password."""
+
     username = StringField("Username", validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField("Password", validators=[InputRequired(), Length(min=4, max=80)])
     remember = BooleanField("Remember me")
 
+
 class RegisterForm(FlaskForm):
+    """ This class uses FlaskForm and creates a form for new users to make an account. An email,
+    username, and password are required. """
+
     email = StringField("Email", validators=[InputRequired(), Email(message="Invalid Email"), Length(max=50)])
     username = StringField("Username", validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField("Password", validators=[InputRequired(), Length(min=4, max=80)])
 
+
 class ActivitiesForm(FlaskForm):
+    """ This class uses FlaskForm and creates a form for users to add new activites. It takes an activity name
+    goal time, both chosen by the user. """
+
     activity_name = StringField("Activity", validators=[InputRequired(), Length(min=1, max=100)])
     goal_time = IntegerField("Time", validators=[InputRequired()])
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    """ A login form is created and prompts the
+    user to login and provides a link for new users so that they can easily signup. """
+
     form = LoginForm()
 
     if form.validate_on_submit():
@@ -71,8 +94,13 @@ def index():
 
     return render_template('index.html', form=form)
 
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    """ A registraction form is created and askes the new user to
+    input a username, email, and password. Then it addes the information to the database and redirects
+    the user back to the login page. """
+
     form = RegisterForm()
 
     if form.validate_on_submit():
@@ -84,10 +112,13 @@ def signup():
 
     return render_template('signup.html', form=form)
 
-@app.route('/dashboard', methods=['GET', 'POST'])
-@login_required
-def dashboard():
-    users_name = current_user.username.capitalize()
+
+def create_complete_list():
+    """ Creates a list of all the activities. Each activity consits of the
+    name, the goal time and the progress time. This data is retreived by
+    quering the databse. There is also an additional element in the list,
+    this just allows the progress bars to appear as different colors when
+    the dashboard template is rendered"""
     activity = Activity.query.filter_by(user_id=str(current_user)).all()
 
     i = 0
@@ -110,41 +141,34 @@ def dashboard():
         complete_list.append(temp_list)
         i += 1
 
+    return complete_list
 
-    # if request.method == 'POST':
-    #     tag = int(request.form['tag'])
-    #     activity[tag].progress += 1
-    #     db.session.commit()
-    #     return redirect(url_for('dashboard'))
 
-    return render_template('dashboard.html', user=users_name, complete_list=complete_list) # activity_list=activity_list, time_goal_list=time_goal_list, current_progress_list=current_progress_list)
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    """ Filtering by the current user, a list of all of
+    the activites  is created. """
+
+    users_name = current_user.username.capitalize()
+    activity = Activity.query.filter_by(user_id=str(current_user)).all()
+
+    complete_list = create_complete_list()
+
+    return render_template('dashboard.html', user=users_name, complete_list=complete_list)
 
 
 @app.route('/add', methods=['GET', 'POST'])
 @login_required
 def add():
+    """ The main purpose of this function is to add an hour to the specified activity. The change is
+    commited to the database and then the user is redirected to the dashboard so that it
+    can update automitcally with the new value in the database. """
+
     users_name = current_user.username.capitalize()
     activity = Activity.query.filter_by(user_id=str(current_user)).all()
 
-    i = 0
-    complete_list = []
-    while i < len(activity):
-        temp_list = []
-        temp_list.append(activity[i].activity_name.capitalize())
-        temp_list.append(activity[i].goal_time)
-        temp_list.append(activity[i].progress)
-        color_determiner = i % 4
-        if color_determiner == 0:
-            color_determiner = 'success'
-        if color_determiner == 1:
-            color_determiner = 'info'
-        if color_determiner == 2:
-            color_determiner = 'warning'
-        if color_determiner == 3:
-            color_determiner = 'danger'
-        temp_list.append(color_determiner)
-        complete_list.append(temp_list)
-        i += 1
+    complete_list = create_complete_list()
 
     if request.method == 'POST':
         tag = int(request.form['tag_a'])
@@ -154,31 +178,18 @@ def add():
 
     return render_template('dashboard.html', user=users_name, complete_list=complete_list)
 
+
 @app.route('/subtract', methods=['GET', 'POST'])
 @login_required
 def subtract():
+    """ This route's main purpose is to subtract an hour to the specified activity. The change is
+    commited to the database and then the user is redirected to the dashboard so that it
+    can update automitcally with the new value in the database. """
+
     users_name = current_user.username.capitalize()
     activity = Activity.query.filter_by(user_id=str(current_user)).all()
 
-    i = 0
-    complete_list = []
-    while i < len(activity):
-        temp_list = []
-        temp_list.append(activity[i].activity_name.capitalize())
-        temp_list.append(activity[i].goal_time)
-        temp_list.append(activity[i].progress)
-        color_determiner = i % 4
-        if color_determiner == 0:
-            color_determiner = 'success'
-        if color_determiner == 1:
-            color_determiner = 'info'
-        if color_determiner == 2:
-            color_determiner = 'warning'
-        if color_determiner == 3:
-            color_determiner = 'danger'
-        temp_list.append(color_determiner)
-        complete_list.append(temp_list)
-        i += 1
+    complete_list = create_complete_list()
 
     if request.method == 'POST':
         tag = int(request.form['tag_s'])
@@ -188,31 +199,18 @@ def subtract():
 
     return render_template('dashboard.html', user=users_name, complete_list=complete_list)
 
+
 @app.route('/clear', methods=['GET', 'POST'])
 @login_required
 def clear():
+    """ This function clears the specified activity. The change is
+    commited to the database and then the user is redirected to the dashboard so that it
+    can update automitcally with the new value in the database. """
+
     users_name = current_user.username.capitalize()
     activity = Activity.query.filter_by(user_id=str(current_user)).all()
 
-    i = 0
-    complete_list = []
-    while i < len(activity):
-        temp_list = []
-        temp_list.append(activity[i].activity_name.capitalize())
-        temp_list.append(activity[i].goal_time)
-        temp_list.append(activity[i].progress)
-        color_determiner = i % 4
-        if color_determiner == 0:
-            color_determiner = 'success'
-        if color_determiner == 1:
-            color_determiner = 'info'
-        if color_determiner == 2:
-            color_determiner = 'warning'
-        if color_determiner == 3:
-            color_determiner = 'danger'
-        temp_list.append(color_determiner)
-        complete_list.append(temp_list)
-        i += 1
+    complete_list = create_complete_list()
 
     if request.method == 'POST':
         tag = int(request.form['tag_c'])
@@ -223,9 +221,34 @@ def clear():
     return render_template('dashboard.html', user=users_name, complete_list=complete_list)
 
 
+
+@app.route('/delete', methods=['GET', 'POST'])
+@login_required
+def delete():
+    """ This function subtracts an hour to the specified activity. The change is
+    commited to the database and then the user is redirected to the dashboard so that it
+    can update automitcally with the new value in the database. """
+
+    users_name = current_user.username.capitalize()
+    activity = Activity.query.filter_by(user_id=str(current_user)).all()
+
+    complete_list = create_complete_list()
+
+    if request.method == 'POST':
+        tag = int(request.form['tag_d'])
+        db.session.delete(activity[tag])
+        db.session.commit()
+        return redirect(url_for('dashboard'))
+
+    return render_template('dashboard.html', user=users_name, complete_list=complete_list)
+
+
 @app.route('/add_activity', methods=['GET', 'POST'])
 @login_required
 def add_activity():
+    """ An activities form is created and
+    commits the new activity and time to the database. """
+
     form = ActivitiesForm()
     if form.validate_on_submit():
         new_activity = Activity(user_id=str(current_user), activity_name=form.activity_name.data, goal_time=form.goal_time.data, progress=0)
@@ -235,9 +258,11 @@ def add_activity():
 
     return render_template('activitiesform.html', form=form)
 
+
 @app.route('/logout')
 @login_required
 def logout():
+    """ This fuctions logs out the current user """
     logout_user()
     return redirect(url_for('index'))
 
